85ee16dc8f42b33eadc1f7bf3d8c38f5cf63915d
terefs <d.van.zurk@gmail.com>
2018-09-03 15:31:26 +0200

    contact page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
 
 class Contacts extends Component {
   render() {
     return (
-    <div>
-      <h1>Contacts page</h1>
-    </div>
-  );
+      <div className="contact-body">
+      <Grid className="contact-grid">
+      <Cell col={6}>
+      <h2>Desmond van Zurk</h2>
+      <img
+      src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+      alt="avatar"
+      style={{height:'250px'}}
+      />
+      <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+      </Cell>
+      <Cell col={6}>
+      <h2>Contact me</h2>
+      <hr/>
+      <div className="contact-list">
+        <List>
+          <ListItem>
+            <ListItemContent style={{fontSize:'30px',fontFamily:'Anton'}}>
+            <i className="fa fa-phone-square" area-hidden="true"/>
+            (123) 456 7890
+            </ListItemContent>
+          </ListItem>
+
+          <ListItem>
+            <ListItemContent style={{fontSize:'30px',fontFamily:'Anton'}}>
+            <i className="fa fa-envelope" area-hidden="true"/>
+            someone@example.com
+            </ListItemContent>
+          </ListItem>
+
+          <ListItem>
+            <ListItemContent style={{fontSize:'30px',fontFamily:'Anton'}}>
+            <i className="fa fa-skype" area-hidden="true"/>
+            Skype-id
+            </ListItemContent>
+          </ListItem>
+          </List>
+      </div>
+
+
+      </Cell>
+      </Grid>
+      </div>
+    );
   }
 }
 
